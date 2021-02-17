541afc0f33e7d825b9a89c8f762a96e4f0197ff0
JacobwBruce <jacobwbruce@hotmail.com>
2021-02-17 16:20:50 -0500

    addPlayer form functionality

--- src/main/java/ca/sheridancollege/repositories/VolleyballController.java
+++ src/main/java/ca/sheridancollege/repositories/VolleyballController.java
@@ -1,8 +1,12 @@
 package ca.sheridancollege.repositories;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import ca.sheridancollege.beans.Player;
 
 @Controller
 public class VolleyballController {
@@ -14,17 +18,23 @@ public class VolleyballController {
 
     @GetMapping("/players")
     public String goPlayers() {
+        // get all players from database
+        // add players to model attribute
         return "players.html";
     }
 
     @GetMapping("/addPlayer")
-    public String goAddPlayer() {
+    public String goAddPlayer(Model model) {
+        model.addAttribute("player", new Player());
         return "addPlayer.html";
     }
 
     @PostMapping("/addPlayer")
-    public String addPlayer() {
-        return "players.html";
+    public String addPlayer(@ModelAttribute Player player) {
+
+        // Save player to database
+
+        return "redirect:/players";
     }
 
 }
